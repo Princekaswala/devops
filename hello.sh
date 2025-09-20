24524b24eb216ce3ec05d045684f254e0eb4f500
princekaswala <co21princekaswala@gmail.com>
2025-09-20 03:24:23 -0400

    File to be pushed

---
+++ hello.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+a=5
+b=10
+c=$((a+b))
+echo "This is the answer : $c" 
+############# 
+# We are adding the subtracion feature in sub branch
+d=$((a-b))
+echo "The answer of subtracion is $d"
+########33333
+# Adding Division feature 
+
+
+
+
+
+
+
+
